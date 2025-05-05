453de19e5cfc936087194a21a4352486eaf412e2
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-05 09:24:46 -0700

    Implemented Time DSL

--- Internal Lab/timeme.py
+++ Internal Lab/timeme.py
@@ -4,4 +4,10 @@ from time import time
 
 @contextmanager
 def timeme(label: str):
-    pass  # TODO (we have ~6 lines)
+    """Context manager that prints how long the wrapped block takes."""
+    start = time()
+    try:
+        yield
+    finally:
+        elapsed = time() - start
+        print(f"{label} took {elapsed:.3f} s")
